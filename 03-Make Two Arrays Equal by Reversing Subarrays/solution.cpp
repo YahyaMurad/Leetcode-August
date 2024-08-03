3fd18a5a88cbe72d785bf465bb965d1244dfe424
YahyaMurad <yahyamurad197@gmail.com>
2024-08-03 03:07:38 +0300

    Added "Make Two Arrays Equal by Reversing Subarrays" cpp solution

---
+++ 03-Make Two Arrays Equal by Reversing Subarrays/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        return target == arr;
+    }
+};
